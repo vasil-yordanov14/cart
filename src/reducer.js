3a9fdecab0be1b31ca570038c2bdb3751ea649b3
vasil-yordanov14 <vasil_sh@abv.bg>
2022-07-26 21:35:06 +0300

    remove items

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+const reducer = (state, action) => {
+  if (action.type === `CLEAR_CART`) {
+    return { ...state, cart: [] };
+  }
+  if (action.type === `REMOVE`) {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+  return state;
+};
+export default reducer;
